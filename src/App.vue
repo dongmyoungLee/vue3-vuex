7fcbf501ea9c17cc98ea24e4665ec496871cd2b6
dongmyounglee <pajang1515@daum.net>
2025-04-02 14:50:42 +0900

    vuex

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import MainComp from './components/MainComp.vue';
+import About from './components/About.vue';
+import {onMounted} from 'vue';
+import {useStore} from 'vuex';
+
+const store = useStore();
+
+// 앱이 실행되면 날씨 데이터 가져오기
+onMounted(() => {
+  store.dispatch('getWeather');
+})
+
+const onSearchCity = (city) => {
+  weatherData.value.city = city;
+  getWeather();
+}
+</script>
+
 <template>
-  <div>
-    z
+  <!-- <button @click="$store.dispatch('getWeather')">getWeather</button> -->
+  <Navbar/>
+  <div v-if="!$store.state.toggle">
+    <MainComp/>
+  </div>
+  <div v-else>
+    <About />
   </div>
 </template>
 
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
+<style scoped lang="scss">
 
 </style>
